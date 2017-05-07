feba51355f73ea1001f27b4f7e3a36ec2197666d
Serdar ŞEN <serdarbote@gmail.com>
2017-05-07 11:43:09 +0300

    damla oyunu yüklendi çalışıyor

--- android/src/com/github/yenitsrm/damlaoyunu/AndroidLauncher.java
+++ android/src/com/github/yenitsrm/damlaoyunu/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.github.yenitsrm.damlaoyunu.Damla Oyunu;
+import com.github.yenitsrm.damlaoyunu.DamlaOyunu;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new Damla Oyunu(), config);
+		initialize(new DamlaOyunu(), config);
 	}
 }
--- desktop/src/com/github/yenitsrm/damlaoyunu/desktop/DesktopLauncher.java
+++ desktop/src/com/github/yenitsrm/damlaoyunu/desktop/DesktopLauncher.java
@@ -2,11 +2,15 @@ package com.github.yenitsrm.damlaoyunu.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.github.yenitsrm.damlaoyunu.Damla Oyunu;
+import com.github.yenitsrm.damlaoyunu.DamlaOyunu;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Damla Oyunu(), config);
+
+		config.width = 800;
+		config.height = 480;
+
+		new LwjglApplication(new DamlaOyunu(), config);
 	}
 }
